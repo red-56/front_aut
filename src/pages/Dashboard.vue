617f937924b6727388a38b6f5d2288d70372c1a7
arnaud <arnaud.sinays@epitech.eu>
2019-09-27 17:06:23 +0200

    feat: user graph and team graph

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -7,42 +7,62 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import DashboardLayout from '@/pages';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import moment from 'moment';
 
 am4core.useTheme(am4themes_animated);
 
 export default {
   name: 'Dashboard',
   mounted() {
-    let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
-
-    chart.paddingRight = 20;
-
-    let data = [];
-    let visits = 10;
-    for (let i = 1; i < 366; i++) {
-      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-      data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
+    let chart = am4core.create(this.$refs.chardiv, am4charts.XYChart);
+    let token = localStorage.getItem('token');
+    if (token == null) {
+      // ya pas de token
     }
+    axios.get('http://localhost:3000/api/workingtimes/user/' +  jwt_decode(token).id, {
+      headers: {Authorization: 'Bearer ' + token}
+      })
+    .then(function(result) {
+      var data = result.data;
 
-    chart.data = data;
+      var graphData = [];
+      var dailyHours = 0;
 
+      var index, obj;
+
+      for (var [index, obj] of data.entries()) {
+        var start = moment(obj.start);
+        var end = moment(obj.end);
+        dailyHours += end.diff(start, 'hours');
+        if (data[index+1] == undefined || !(moment(data[index+1].start).isSame(start, 'day'))) {
+           graphData.push({
+            date: `${start.year().toString()}-${(start.month() + 1).toString()}-${start.date().toString()}`,
+            hours: dailyHours
+          });
+          dailyHours = 0;
+        }
+      }
+      chart.data = graphData;
+    })
+    // XY
+    chart.dateFormatter.dateFormat = "yyyy-MM-dd";
     let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-    dateAxis.renderer.grid.template.location = 0;
-
     let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.tooltip.disabled = true;
-    valueAxis.renderer.minWidth = 35;
-
-    let series = chart.series.push(new am4charts.LineSeries());
+    let series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.valueY = "hours";
     series.dataFields.dateX = "date";
-    series.dataFields.valueY = "value";
+    series.tooltipText = "{value}";
+    series.dataFields.value = "hours";
+    series.columns.template.width = am4core.percent(50);
+    series.tooltip.pointerOrientation = "vertical";
 
-    series.tooltipText = "{valueY.value}";
     chart.cursor = new am4charts.XYCursor();
-
-    let scrollbarX = new am4charts.XYChartScrollbar();
-    scrollbarX.series.push(series);
-    chart.scrollbarX = scrollbarX;
+    chart.cursor.snapToSeries = series;
+    chart.cursor.xAxis = dateAxis;
+    //chart.scrollbarY = new am4core.Scrollbar();
+    chart.scrollbarX = new am4core.Scrollbar();
 
     this.chart = chart;
   },
--- src/pages/TeamGraph.vue
+++ src/pages/TeamGraph.vue
@@ -1,17 +1,100 @@
 <template>
-
+  <div class="hello" ref="chartdiv"></div>
 </template>
 
 <script>
-export default {
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import DashboardLayout from '@/pages';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import moment from 'moment';
 
+am4core.useTheme(am4themes_animated);
+
+export default {
   name: 'TeamGraph',
-  
-  props: {
-    dataBackgroundColor: {
-      type: String,
-      default: ""
+  mounted() {
+    let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+    let token = localStorage.getItem('token');
+    if (token == null) {
+      // ya pas de token
     }
+    let teamid = 1;
+
+    axios.get('http://localhost:3000/api/workingtimes/team/' +  teamid, {
+      headers: {Authorization: 'Bearer ' + token}
+      })
+    .then(function(result) {
+      var data = result.data;
+
+      var graphData = [];
+      var employees = [];
+      var dailyHours = 0;
+      var index, obj;
+
+      data.forEach(obj => {
+        console.log(obj);
+        if (!(obj.employeeId in employees)) {
+          employees.push(obj.employeeId);
+        }
+      });
+      console.log(employees);
+      for (var [index, obj] of data.entries()) {
+        var start = moment(obj.start);
+        var end = moment(obj.end);
+        dailyHours += end.diff(start, 'hours');
+        if (data[index+1] == undefined || !(moment(data[index+1].start).isSame(start, 'day'))) {
+           graphData.push({
+            date: `${start.year().toString()}-${(start.month() + 1).toString()}-${start.date().toString()}`,
+            hours: dailyHours / employees.length
+          });
+          dailyHours = 0;
+        }
+      }
+      chart.data = graphData;
+    })
+    chart.dateFormatter.dateFormat = "yyyy-MM-dd";
+    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+    let series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.valueY = "hours";
+    series.dataFields.dateX = "date";
+    series.tooltipText = "{value}";
+    series.dataFields.value = "hours";
+    series.columns.template.width = am4core.percent(50);
+
+    series.tooltip.pointerOrientation = "vertical";
+
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.snapToSeries = series;
+    chart.cursor.xAxis = dateAxis;
+
+    //chart.scrollbarY = new am4core.Scrollbar();
+    chart.scrollbarX = new am4core.Scrollbar();
+
+    this.chart = chart;
+  },
+
+  beforeDestroy() {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  },
+
+  components: {
+    DashboardLayout
   }
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  width: 100%;
+  height: 500px;
+}
+</style>
