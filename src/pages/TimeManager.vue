2d64ac9bfa000b2379bab04fbb01903360fe5fb2
arnaud <arnaud.sinays@epitech.eu>
2019-09-30 22:05:50 +0200

    fix: clock_in&clock_out

--- src/pages/TimeManager.vue
+++ src/pages/TimeManager.vue
@@ -2,9 +2,18 @@
     <div>
         <p> <b>&nbsp;&nbsp;&nbsp;{{ today }}</b></p>
         <center><b-jumbotron header="Pointez" lead="Pensez à pointer à votre arrivée, et dès votre départ." class="center" border-variant="dark"><br>
-            <b-button variant="primary" v-on:click="postClock" v-if="!statusClock">Démarrer</b-button>
-            <b-button variant="primary" v-on:click="postClock" v-if="statusClock">{{chrono}} | stop</b-button><br><br>
-            <b><span class="float-left">Arrivé(e): {{ arrival }}</span></b><b><span class="float-right">Parti(e): {{ departure }}</span></b>
+            <b-button variant="primary" v-on:click="patchClock">Pointer</b-button><br/><br/>
+            <div v-if="statusClock">
+                <br/>
+                <p>Vous êtes actuellement: Au travail</p>
+                <b><span>Dernière Arrivée: <br/><b>{{ timeClock }}</b></span></b>
+            </div>
+            <div v-if="!statusClock">
+                <br/>
+                <p>Vous êtes actuellement: En dehors</p>
+                <b><span>Dernier Départ:<br/><b>{{ timeClock }}</b></span></b>
+            </div>
+           
         </b-jumbotron></center><br><br>
         <b-card bg-variant="light" text-variant="dark" border-variant="dark" title="Récapitulatif horaire d'aujourd'hui">
             <b-card-text>
@@ -13,14 +22,14 @@
             <b-card-text>
                 Parti(e): <date-picker format="YYYY-MM-DD HH:mm:ss" valueType="format" v-model="setDeparture" type="datetime" :time-picker-options="timeOptions" :lang="lang" :shortcuts="shortcuts"></date-picker>
             </b-card-text>
-            <center><b-button href="#" variant="primary" v-on:click="setWorkingTime">Mettez à jours vos horaires</b-button></center>
+            <center><b-button href="#" variant="primary" v-on:click="setWorkingTime">Mettez à jour vos horaires</b-button></center>
         </b-card>
     </div>
 </template>
 
 <script>
 
-import moment from 'moment';
+import moment, { duration } from 'moment';
 import { setInterval, clearInterval } from 'timers';
 import axios from 'axios';
 import Swal from 'sweetalert2';
@@ -29,14 +38,11 @@ import jwt_decode from 'jwt-decode';
 import DatePicker from 'vue2-datepicker'
 
 export default {
-    
     name: 'TimeManager',
-
     components: {
         datetime,
         DatePicker
     },
-
     data() {
         return {
             shortcuts: [
@@ -63,78 +69,67 @@ export default {
             arrival: null,
             departure: null,
             statusClock: false,
+            timeClock: null,
             finalChrono: null,
             setDeparture: null,
-            setArrival: null,
-            clocks: {
-                status: '',
-                time: ''
-            }
+            setArrival: null
         }
     },
-    created(){
-
-    },
+    mounted() {
+        this.getClock()
+        .then((data) => {
+            this.statusClock = data.status
+            this.timeClock = moment.utc(data.time).format('YYYY-MM-DD HH:mm:ss')
+        })
+    }
+    ,
     methods:{
-        postClock(){
-
-            if (this.chrono == null){
-
-                this.statusClock = true;
-                const now = moment(moment.now()).add(1, 'hours');
-                this.arrival = moment().toISOString().substring(0, moment().toISOString().length -1)
-                this.clocks = {
-                    status: true,
-                    time: moment().toISOString().substring(0, moment().toISOString().length -1)
-                };
-
-                axios.patch('https://timepool.me:3001/api/clocks/user/' + jwt_decode(localStorage.getItem('token')).id,  this.clocks, {
-                    headers: {
-                        Authorization: 'Bearer ' + localStorage.getItem('token')
-                    }
+        getClock(){
+            return new Promise((resolve) => {
+                var token = localStorage.getItem('token')
+                var query = "https://timepool.me:3001/api/clocks/user/" + jwt_decode(token).id;
+                var self = this;
+                axios.get(query, {
+                    headers: {Authorization: 'Bearer ' + token}
                 })
-                .then((response) => {
-
+                .then(function(result) {
+                    resolve(result.data);
                 })
                 .catch((error) => {
                     console.log(error);
                 });
-                
-                this.interval = setInterval(()=> {
-                    this.chrono = moment(moment().diff(now)).format("HH:mm:ss")
-                }, 1000);
-
-            }else {
-                this.finalChrono = this.chrono;
-                
-                this.chrono = null;
-                this.statusClock = false;
-                this.departure = moment().toISOString().substring(0, moment().toISOString().length -1);
-                   this.clocks = {
-                    status: false,
-                    time: moment().toISOString().substring(0, moment().toISOString().length -1)
-                };
-
-                axios.patch('https://timepool.me:3001/api/clocks/user/' + jwt_decode(localStorage.getItem('token')).id,  this.clocks, {
-                    headers: {
-                        Authorization: 'Bearer ' + localStorage.getItem('token')
-                    }
-                })
-                .then((response) => {
-
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
-
-                clearInterval(this.interval);
-                Swal.fire("Vous avez travaillé " + this.finalChrono);
-            }
-
+            }); 
         },
-
+        patchClock(){
+            var token = localStorage.getItem('token');
+            var self = this;
+            var oldStatus = self.statusClock;
+            self.statusClock = !self.statusClock;
+            var timeWhenClicked = moment().format('YYYY-MM-DD[T]HH:mm:ss.SSS[Z]');
+            console.log(timeWhenClicked);
+            var clockObject = {status: self.statusClock, time: timeWhenClicked};
+            axios.patch('https://timepool.me:3001/api/clocks/user/' + jwt_decode(token).id, clockObject, {
+                    headers: {
+                            Authorization: 'Bearer ' + localStorage.getItem('token')
+                    }
+            })
+            .then(function() {  
+                if(oldStatus) {
+                    var timeWorked = moment.utc(timeWhenClicked).diff(moment.utc(self.timeClock));
+                    var hours = parseInt(moment.duration(timeWorked).asHours());
+                    var minutes = parseInt(moment.duration(timeWorked).asMinutes())%60;
+                    Swal.fire(`Vous avez travaillé ${hours} H et ${minutes} m`);
+                }
+                self.getClock()
+                .then(data => {
+                    self.timeClock = moment.utc(data.time).format('YYYY-MM-DD HH:mm:ss')
+                })
+            })  
+            .catch((error) => {
+                console.log(error);
+            });      
+        },
         setWorkingTime() {
-
             if (this.chrono == null && (this.setDeparture == null || this.setArrival == null)) {
                 Swal.fire({
                     type: 'error',
@@ -200,13 +195,7 @@ export default {
                     footer: 'Merci de réessayez'
                 })
             }
-
-        }
     }
-
-
-
-
-
+}
 }
 </script>
